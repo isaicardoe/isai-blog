9bed6a6883b09289f7f9e54680403835de302e62
isai cardoso <isai.cardoe@gmail.com>
2014-10-16 12:29:34 -0700

    yea

--- controller/create_db.php
+++ controller/create_db.php
@@ -2,3 +2,10 @@
   require_once ("../model/database.php");
   
   $conection = new mysqli($host, $username, $password);
+  
+  if($connection->connect_error) {
+      
+  }
+  else {
+      echo "Success";
+  }
