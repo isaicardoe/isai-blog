b80c2c5f6125053890adb850baab0c1a9bb5c170
isai cardoso <isai.cardoe@gmail.com>
2014-10-28 11:48:21 -0700

    comments

--- controller/create_db.php
+++ controller/create_db.php
@@ -1,23 +1,25 @@
 <?php
   require_once(__DIR__ . "/../model/database.php");
-  
+  //it is checking if the file exists in the model folder
   $connection = new mysqli($host, $username, $password);
-  
+  //checking if the host and stuff is their
   if($connection->connect_error) {
       die("Error: " . $connection->connect_error);
   }
-  
+  //
   $exists = $connection->select_db($database);
-  
+  //
   if(!$exists) {
       $query = $connection->query("CREATE DATABASE $database");
-      
+  //  
       if($query) {
           echo "Successfully created database: " . $database;
+  //
       }
   }
     else {
       echo "Database already exists";
+  //
     }
     
     $query = $connection->query("CREATE TABLE posts {"
@@ -25,5 +27,6 @@
             . "title varchart(255) NOT NULL"
             . "post text NOT NULL,"
             . "PRIMARY KEY (id)");
-  
+  //
   $connection->close();
+  //
