8faf57a728fefd67b76fd0aa2d3f310f7be7010d
isai cardoso <isai.cardoe@gmail.com>
2014-11-03 13:26:56 -0800

    comenting

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
     require_once(__DIR__ . "/view/header.php");
+    //getting the header php file out the folder
     require_once(__DIR__ . "/controller/create_db.php");
+    //getting the create php file out the controller folder
      require_once(__DIR__ . "/view/footer.php");
-    
+    //getting the footer php file out the folder
 ?>
