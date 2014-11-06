fc4c380bf955c0ecaba516ada9543df2ddbc9f96
isai cardoso <isai.cardoe@gmail.com>
2014-11-06 12:14:58 -0800

    yo dan

--- view/header.php
+++ view/header.php
@@ -1,6 +1,7 @@
 
 <html>
     <head>
+        <meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="main.css">
         <title>Isai Blog</title>
