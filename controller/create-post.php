6355e49e8cfdf581c50885b954dc33e0cf721817
isai cardoso <isai.cardoe@gmail.com>
2014-11-07 11:12:42 -0800

    comenting

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,6 +1,7 @@
 <?php
  $title = filer_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
  $post = filer_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
-
+ //making varbles and saving them in the postand title varibles we created
  echo "<p>Title: $title</p>";
  echo "<p>Post: $post</p>";
+ //echoing that it was saved in the varibles we created
