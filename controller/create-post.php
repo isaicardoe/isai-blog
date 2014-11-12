e0c897e8ab8bb1a90c665edc287c858fa23207b0
isai cardoso <isai.cardoe@gmail.com>
2014-11-12 12:23:37 -0800

    saving the comments the in varibles we created

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,7 +1,17 @@
 <?php
+ require_once(__DIR__ . "/../model/database.php");
+ //getting the database php file out of the model folder
+ 
+ $connection = new mysqli($host, $username, $password, $database);
+ //getting all the userstuff and saving them in the varibles
+ 
  $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
  $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
  //making varbles and saving them in the postand title varibles we created
+ 
  echo "<p>Title: $title</p>";
  echo "<p>Post: $post</p>";
- //echoing that it was saved in the varibles we created
+ //echoing that it was saved in the varibles we created 
+ 
+ $connection->close();
+ //it stops the connection varble that was running
