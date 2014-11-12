cd13b9ee5cec03d5827afa6ffe55683c7afb311a
isai cardoso <isai.cardoe@gmail.com>
2014-11-12 12:08:39 -0800

    made the page that when you hit the submit button it make a page and puts it on the page

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,6 +1,6 @@
 <?php
- $title = filer_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
- $post = filer_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+ $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
+ $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
  //making varbles and saving them in the postand title varibles we created
  echo "<p>Title: $title</p>";
  echo "<p>Post: $post</p>";
--- view/form.php
+++ view/form.php
@@ -1,6 +1,11 @@
+<?php
+    require_once(__DIR__ . "/../model/config.php");
+    //getting the config php file out of the folder called model 
+?>
+
 <h1>Create Blog Post</h1>
 
-<form>
+<form method="post" action="<?php echo $path . "controller/create-post.php"; ?>">
     <div>
         <label for="title">Title: </label>
         <input type="text" name="title" />
