42ec873ba7eedf3f9d16033e7e5f3de7eb7324cf
isai cardoso <isai.cardoe@gmail.com>
2014-11-13 12:06:55 -0800

    created and saved the post and title info in the string

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,17 +1,24 @@
 <?php
- require_once(__DIR__ . "/../model/database.php");
- //getting the database php file out of the model folder
+  require_once(__DIR__ . "/../model/database.php");
+  //getting the database php file out of the model folder
  
- $connection = new mysqli($host, $username, $password, $database);
- //getting all the userstuff and saving them in the varibles
+  $connection = new mysqli($host, $username, $password, $database);
+  //getting all the userstuff and saving them in the varibles
  
- $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
- $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
- //making varbles and saving them in the postand title varibles we created
+  $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
+  $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+  //making varbles and saving them in the postand title varibles we created
  
- echo "<p>Title: $title</p>";
- echo "<p>Post: $post</p>";
- //echoing that it was saved in the varibles we created 
+  $query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'");
+  //you are creating a title and post database to save in the query table we created and the connection 
+  //is inserting the pot and title inside the table and storing it
  
- $connection->close();
- //it stops the connection varble that was running
+  if($query) {
+      echo "<p>Successfully inserted post: $title</p>";  
+  }
+  else {
+      echo "<p>$connection->error</p>";
+  }
+ 
+  $connection->close();
+  //it stops the connection varble that was running
--- model/database.php
+++ model/database.php
@@ -1,6 +1,1 @@
 <?php
-   $host = "localhost";
-   $username = "root";
-   $password = "root";
-   $database = "blog_db";
-   //this is all the host information 
