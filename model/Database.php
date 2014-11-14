196bb9bf6cae9b794f08ab2c7f4a685d56cae8e1
isai cardoso <isai.cardoe@gmail.com>
2014-11-14 12:22:16 -0800

    understanding why we use classes instead of lots of varibles

--- model/Database.php
+++ model/Database.php
@@ -5,4 +5,13 @@
       private $username;
       private $password;
       private $database;
+      //private stuff for your database like your password,and others above 
+      //only for the host user stuff
+      //we created this class beacause it is easier to read and manage and way cleaner
+      //it is better for blobs of code like this instead of writing a lot of code and it is easier 
+      //to call and when you call it it runs faster and slimpler for the user to use and you can use over and over 
+      //instead of writing it like 10 times 
+      //when you call it again it makes a new instance of a consistant object 
+      //makes it a global varible so we can access then whenver
+      //you want to
   }
